11db0f286e514aec9aae21ff204c1a3a0a4b0f85
egor.pisarev <egor.pisarev@gmail.com>
2024-05-25 17:18:45 +0400

    chat

--- chat.js
+++ chat.js
@@ -4,9 +4,9 @@ const openai = new OpenAI({
   apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
 });
 
-async function send(content) {
+async function send(messages) {
   const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: "user", content }],
+    messages,
     model: "gpt-3.5-turbo",
   });
 
--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const chats = {};
 const bot = new Telegraf(process.env.BOT_TOKEN);
 //save session by chatId
 bot.start((ctx) => {
-  if (chats[ctx.chat.id]) {
+  if (!chats[ctx.chat.id]) {
+    chats[ctx.chat.id] = {
+      messages: [{ role: "system", content: 'Давай общаться на русском языке. Об nft ton' }]
+    }
   }
-  console.log(ctx.message.from);
   ctx.reply("Welcome");
 });
 bot.help((ctx) => ctx.reply("Send me a sticker"));
@@ -19,7 +21,13 @@ bot.hears("hi", (ctx) => {
   ctx.reply("Welcome");
 });
 bot.on("message", async (ctx) => {
-  const result = await chat.send(ctx.update.message.text);
+  if (!chats[ctx.chat.id]) {
+    chats[ctx.chat.id] = {
+      messages: [{ role: "system", content: 'Давай общаться на русском языке. Об nft ton' }]
+    }
+  }
+  chats[ctx.chat.id].messages.push({role: 'user', content: ctx.update.message.text})
+  const result = await chat.send(chats[ctx.chat.id].messages);
   ctx.reply(result.choices[0].message.content);
 });
 bot.launch();
