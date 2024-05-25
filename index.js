fc018b2c524657b7a995d7672bb8ffa977c14d26
Matvei <matvei.cherntsov@gmail.com>
2024-05-25 13:10:52 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+
+const { Telegraf } = require('telegraf')
+
+const chats = {}
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+//save session by chatId
+bot.start((ctx) => {
+    console.log(ctx.message.from)
+    ctx.reply('Welcome')
+})
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => {
+        console.log(ctx.message.from)
+        ctx.reply('Welcome')
+})
+bot.launch()
