8ebebbef2650c904fae54054e97cd025e098ba3c
Matvei <matvei.cherntsov@gmail.com>
2024-05-25 14:37:37 +0400

    commit

--- index.js
+++ index.js
@@ -3,24 +3,19 @@ require('dotenv').config()
 
 const { Telegraf } = require('telegraf')
 
-const chats = {}
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 //save session by chatId
 bot.start((ctx) => {
-    const chatId = ctx.chat.id.toString(); // Convert chat id to string
-    if (!chats[chatId]) {
-        chats[chatId] = {
-            // Define initial properties if needed
-        };
-        console.log(`New chat session started for chat id: ${chatId}`);
+    const userId = ctx.message.from.id;
+    if(chats[userId]){
+        ctx.reply('Already Exist')
     } else {
-        console.log(`Session already exists for chat id: ${chatId}`);
+        ctx.reply('New comer')
     }
-    console.log(ctx.message.from)
-    console.log(chats)
-    ctx.reply('Welcome')
 })
+
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => {
